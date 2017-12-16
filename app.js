4216671afd18b95c156c338d278d4b974e355139
Solonel <30692287+Solonel@users.noreply.github.com>
2017-12-16 15:55:20 +0100

    Node.js, Express.js, JWT et Mongoose par la pratique
    Chapitre 10. Découper un template en 'partials'
    Chapitre 11. Pratiquer la création de 'partials'
    Chapitre 12. Gérer l'envoi de fichiers statiques
    Chapitre 13. Passer des données complexes à une vue et itérer côté template
    Chapitre 14. Créer un formulaire et gérer le POST
    Chapitre 15. Récupérer le body d’une requête à l’aide du middleware body-parser
    Chapitre 16. Gérer la persistance en mémoire
    Chapitre 17. Poster un formulaire en Ajax
    Chapitre 18. Poster un formulaire via le middleware ‘multer’
    Chapitre 19. Poster un formulaire à l’aide du ‘bon vieux’ XHR
    Chapitre 20. Présentation de l’API de ‘The Movie DB’
    Chapitre 21. Faire une requête à l’aide de axios
    Chapitre 22. Parser les résultats et les intégrer dans une page web

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
 
 const PORT = 3000;
+
+let frenchMovies = [];
 
 app.use('/public', express.static('public'));
 
@@ -9,16 +14,56 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/movies', (req, res) => {
-    res.render('movies');
+    const title = "FrenchMovies";
+
+    frenchMovies =   [{ title: "Toto 1", year: "1" },
+    { title: "Toto 2", year: "2" },
+    { title: "Toto 3", year: "3" },
+    { title: "Toto 4", year: "4" },
+    { title: "Toto 5", year: "5" },
+    { title: "Toto 6", year: "6" }];
+
+    res.render('movies', { frenchMovies: frenchMovies, title: title });
 });
 
-app.get('/movies/add', (req, res) => {
-    res.render('add-movie');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+//app.post('/movies', urlencodedParser, (req, res) => {
+//    const newMovie = { title: req.body.movieTitle, year: req.body.movieYear };
+//    frenchMovies = [...frenchMovies, newMovie];
+//    res.sendStatus(201);
+//});
+
+app.post('/movies-fetch', upload.fields([]), (req, res) => {
+    if (req.body) {
+        const formData = req.body;
+        const newMovie = { title: formData.movieTitle, year: formData.movieYear };
+        frenchMovies = [...frenchMovies, newMovie];
+        console.log('formData', formData);
+        res.sendStatus(201);
+    } else {
+        return res.sendStatus(500);
+    }
 });
+
+app.post('/movies-xhr', urlencodedParser, (req, res) => {
+    if (!req.body) {
+        return res.sendStatus(500);
+    } else {        
+        frenchMovies = [... frenchMovies, { title: req.body.movietitle, year: req.body.movieyear }];
+        console.log (req.body);
+        res.sendStatus(201);
+    } 
+});
+
 
 app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
-    res.render('movie-details', { movie_id : id });
+    res.render('movie-details', { movie_id: id });
+});
+
+app.get('/search', (req, res) => {
+    res.render('movies-search');
 });
 
 app.get('/', (req, res) => {
